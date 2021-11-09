07ee7ca98bdd605e2e78716911d20ae14a1b3566
Ko3uHaK <den2001888@yandex.by>
2021-11-09 13:00:03 +0300

    added obj and mas

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,7 @@ const toDoForm = document.querySelector('.js-toDoForm'),
       toDoInput = toDoForm.querySelector('input'),
       toDoList = document.querySelector(".js-toDoList");
 const TODO_LS ='toDo';
+const toDos =[];
       function loadToDo(){
           const todo = localStorage.getItem(TODO_LS);
           if(todo !== null){
@@ -18,6 +19,11 @@ function showToDo(text){
     li.appendChild(delBtn);
     li.appendChild(span);
     toDoList.appendChild(li);
+    const toDoobj ={
+        name: text,
+        id: toDos.length +1
+    };
+    toDos.push(toDoobj);
 }
 
 function submitHandler(event){
