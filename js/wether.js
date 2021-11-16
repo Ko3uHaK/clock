c8963f18850d035da4f4e2d9774f69c692ecd298
Ko3uHaK <den2001888@yandex.by>
2021-11-16 17:23:36 +0300

    finish wether

--- js/wether.js
+++ js/wether.js
@@ -1,15 +1,19 @@
-const COORDS_LS = "coords",
-  API_KEY = "847633b9fcbea4c7f750a788b3cd735c";
+const COORDS_LS = "coords";
+const API_KEY = "847633b9fcbea4c7f750a788b3cd735c";
 
-function getWether(lat, lon){
-    fetch(`https://api.openweathermap.org/data/2.5/
-    weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
-    .then(function(response){
-        return response.json();
-        })
-        .then(function(json){
+const weatherContainer = document.querySelector(".js-weather");
 
-        });
+function getWether(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weatherContainer.innerText = `${temperature}℃ 
+       ${place}`;
+    });
 }
 
 function saveCoords(positionObj) {
@@ -23,27 +27,30 @@ function geoSuccessHandler(position) {
     latitude: latitude,
     longitude: longitude,
   };
-  saveCoords();
+  saveCoords(positionObj);
   getWether(latitude, longitude);
 }
 
-function geoErrorHandler() {}
+function geoErrorHandler() {
+  alert("Ошибка определения координат");
+}
 
 function askForCoords() {
   navigator.geolocation.getCurrentPosition(geoSuccessHandler, geoErrorHandler);
 }
 
 function getCoords() {
-  const coords = localStorage.getItem();
+  const coords = localStorage.getItem(COORDS_LS);
   if (coords === null) {
     askForCoords();
   } else {
-      const loadedCoords = JSON.parse(coords);
-      getWether(loadedCoords.latitude, loadedCoords.longitude);
+    const loadedCoords = JSON.parse(coords);
+    getWether(loadedCoords.latitude, loadedCoords.longitude);
   }
 }
 
 function init() {
   getCoords();
 }
+
 init();
